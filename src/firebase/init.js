d791fe2dc8fd4cc77456ffe20f9f54e7ef8bb088
Matthew Ritter <mritter2283@gmail.com>
2019-02-12 19:46:41 -0500

    started a new project added firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDbHlZD0D24ty9FcbUxww8DXpaWZywqDTA",
+  authDomain: "chat-app-ef206.firebaseapp.com",
+  databaseURL: "https://chat-app-ef206.firebaseio.com",
+  projectId: "chat-app-ef206",
+  storageBucket: "chat-app-ef206.appspot.com",
+  messagingSenderId: "775491232714"
+};
+const firebaseApp = firebase.initializeApp(config);
+export default firebaseApp.firestore();
