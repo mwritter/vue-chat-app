e31bb100b567071439f67b98d7aaac2c10ad4839
Matthew Ritter <mritter2283@gmail.com>
2019-02-12 20:25:42 -0500

    Added style to chat component with input

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="chat container">
-    <h2>Chat</h2>
+    <h2 class="center teal-text">Chat</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li>
+            <span class="teal-text">Name</span>
+            <span class="grey-text text-darken-3">message</span>
+            <span class="grey-text time">time</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <input type="text">
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -13,4 +27,15 @@ export default {
 };
 </script>
 <style>
+.chat h2 {
+  font-size: 2.6em;
+  margin-bottom: 40px;
+}
+.chat span {
+  font-size: 1.4em;
+}
+.chat .time {
+  display: block;
+  font-size: 1.2em;
+}
 </style>
